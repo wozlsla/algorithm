44218e78fd03d3752d909b0d97034148c6dedf54
jimin <jiminseo0307@gmail.com>
2024-11-19 10:54:07 +0900

    base: bubble sort

---
+++ sparta/week4/sort.py
@@ -0,0 +1,6 @@
+def bubblesort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
---
+++ sparta/week4/test.py
@@ -0,0 +1,6 @@
+from sort import bubblesort
+
+
+# 버블 정렬
+assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]

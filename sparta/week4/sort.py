0a4f49577e941fd15c71129dfaf4cbf27a27c010
jimin <jiminseo0307@gmail.com>
2024-11-19 11:26:32 +0900

    base: selection sort

--- sparta/week4/sort.py
+++ sparta/week4/sort.py
@@ -4,3 +4,14 @@ def bubblesort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+
+def selectionsort(arr):
+    for i in range(len(arr) - 1):
+        min = i
+        for j in range(i + 1, len(arr)):
+            if arr[min] > arr[j]:
+                min = j
+        if min != i:
+            arr[min], arr[i] = arr[i], arr[min]
+    return arr
--- sparta/week4/test.py
+++ sparta/week4/test.py
@@ -1,6 +1,11 @@
-from sort import bubblesort
+from sort import bubblesort, selectionsort
 
 
 # 버블 정렬
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+
+# 선택 정렬
+assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]

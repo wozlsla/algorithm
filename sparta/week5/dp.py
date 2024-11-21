5b1198992671d79fe82d6cf42f63bce77c3d9904
jimin <jiminseo0307@gmail.com>
2024-11-21 13:56:01 +0900

    base: 극장 좌석 자리 구하기

--- sparta/week5/dp.py
+++ sparta/week5/dp.py
@@ -20,3 +20,30 @@ def fibo_dp(n):
     memo[n] = fibo_dp(n - 1) + fibo_dp(n - 2)
 
     return memo[n]
+
+
+# 극장 좌석 자리 구하기
+def cal_seat(n, vips):
+    res = 1
+
+    memo = {
+        0: 1,
+        1: 1,
+        2: 2,
+    }
+
+    def dp(n):
+        if n in memo:
+            return memo[n]
+        memo[n] = dp(n - 1) + dp(n - 2)
+        return memo[n]
+
+    # 1 2 {3} 4 5 {6} 7 8 9
+    bf_vip = 0
+    for vip in vips:
+        cnt = dp(vip - 1 - bf_vip)
+        res *= cnt
+        bf_vip = vip
+
+    res *= dp(n - bf_vip)
+    return res
--- sparta/week5/test_dp.py
+++ sparta/week5/test_dp.py
@@ -1,10 +1,17 @@
-from dp import fibo, fibo_dp
+from dp import fibo, fibo_dp, cal_seat
 
-# Recursive
+# Recursive (fibo)
 assert fibo(10) == 55
 # assert fibo(100) == 354224848179261915075
 
 
-# DP
+# DP (fibo)
 assert fibo_dp(10) == 55
 assert fibo_dp(100) == 354224848179261915075
+
+
+# 극장 좌석 자리 구하기
+assert cal_seat(9, [4, 7]) == 12
+assert cal_seat(9, [2, 4, 7]) == 4
+assert cal_seat(11, [2, 5]) == 26
+assert cal_seat(10, [2, 6, 9]) == 6
